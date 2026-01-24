698171fd71e4553f5caee5f39eecb1e63647906d
sylvi <20102981@mail.wit.ie>
2026-01-24 15:37:57 +0000

    Fix all errors

--- middleware/api/users/index.js
+++ middleware/api/users/index.js
@@ -1,16 +1,28 @@
 import express from 'express';
 import User from './userModel';
+import asyncHandler from 'express-async-handler'
+
 
 const router = express.Router()
 
-//1. Register a user
-router.post('/', async (req, res) => {
-    await User(req.body).save();
-    res.status(201).json({
-        code: 201,
-        msg: 'Successfully created new user.'
-    });
-});
+//1. Register a user or authenticate a user
+router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {
+        await User(req.body).save();
+        res.status(201).json({
+            code: 201,
+            msg: 'Successfully created new user.'
+        });
+    }
+    else {
+        const user = await User.findOne(req.body);
+        if (!user) {
+            return res.status(401).json({ code: 401, msg: "Authentication Failed"});
+        }else{
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+        }
+    }
+}));
 
 //2. Get all users
 router.get('/', async (req, res) => {
