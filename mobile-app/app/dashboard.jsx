e1d507b25237a5406e01e262529d5c25125cc82c
sylvi <20102981@mail.wit.ie>
2026-01-31 17:51:14 +0000

    Create the symptoms text input

--- mobile-app/app/dashboard.jsx
+++ mobile-app/app/dashboard.jsx
@@ -1,26 +1,11 @@
-
-import { useState } from 'react';
 import { View, Text} from 'react-native'
-import Symptom from '../components/Symptom';
 
-export default function Dashboard() {
-  const [ symptomState, setSymptomState ] = useState({
-          symptoms: [
-            { id:1, name:"cramps" },
-            { id:2, name:"muscle aches" },
-            { id:3, name:"irritability" }
-          ]
-         });    
+export default function Dashboard() {   
 
   return (
     <View>
         <Text>Dashboard</Text>
-        {symptomState.symptoms.map((symptom) => (
-          <Symptom
-            name={symptom.name}
-            key={symptom.id}
-            />
-        ))}
+        
     </View>
   );
 }
--- mobile-app/app/log.jsx
+++ mobile-app/app/log.jsx
@@ -1,15 +1,43 @@
-import { View, Text} from 'react-native'
+import { View, Text, TextInput, Button, Pressable} from 'react-native'
+import { colours } from '../theme/colours'
+import { useState } from 'react';
 
 export default function Log() {
+  const [date, setDate] = useState('');
+  const [symptoms, setSymptoms] = useState('');
+
+  const handleSubmit = () => {
+    console.log({
+      date,
+      symptoms
+    });
+  };
+
+
   return (
-    <View>
-        <Text>Log</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 16, backgroundColor: colours.background.default }}>
+        {/* Date input */}
+        <Text> Date</Text>
+        <TextInput 
+            value={date}
+            onChangeText={setDate}
+            placeholder="YYYY-MM-DD"
+            style={{ borderWidth: 1, marginBottom: 10, padding: 6 }}
+        />
 
          {/* Last period date input */}
 
          {/* Cycle length input */}
 
          {/* Symptoms input*/}
+         <Text> Symptoms</Text>
+         <TextInput 
+            value={symptoms}
+            onChangeText={setSymptoms}
+            placeholder="Describe symptoms"
+            style={{ borderWidth: 1, marginBottom: 10, padding: 6 }}
+          />
+
 
          {/* Mood input */}
 
@@ -18,6 +46,13 @@ export default function Log() {
          {/* Supplements input */} 
 
          {/* Hormones input*/}
+
+         {/* Submit button*/}
+         <Pressable onPress={handleSubmit} 
+          style={{ backgroundColor: colours.primary.main, paddingVertical: 10, paddingHorizontal: 20, borderRadius: 5 }}>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>Save</Text>
+          </Pressable>
+
     </View>
   );
 }
