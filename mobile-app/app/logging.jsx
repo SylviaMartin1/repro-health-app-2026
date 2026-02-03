3483ca2621ec398a73262b07941fecb42fe26dc4
sylvi <20102981@mail.wit.ie>
2026-02-03 10:50:09 +0000

    Add state for date and cycle length to form page

--- mobile-app/app/logging.jsx
+++ mobile-app/app/logging.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import { View, Text} from 'react-native'
 
 export default function Logging() {
+  const [date, setDate] = useState("")
+  const [cycleLength, setCycleLength] = useState("")
+
+
   return (
     <View>
         <Text>Logging</Text>
