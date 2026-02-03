a0a7e6d85942ac7cec5c5fc1eac37d0fb085614b
sylvi <20102981@mail.wit.ie>
2026-02-03 10:35:36 +0000

    Add state for the logs

--- mobile-app/app/dashboard.jsx
+++ mobile-app/app/dashboard.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { View, Text} from 'react-native'
 import Log from '../components/Log';
 
 export default function Dashboard() {
+  const [ logState, setLogState] = useState({
+    logs: [
+      { date: "11-12-2024", cycleLength:"28"},
+      { date: "16-12-2024", cycleLength:"26"},
+      { date: "13-12-2024", cycleLength:"32"}
+    ]
+  })
+
+
   return (
     <View>
         <Text>Dashboard</Text>
-        <Log date="11-12-2026" cycleLength="28"/>
-        <Log date="15-12-2026" cycleLength="26"/>
-        <Log date="13-12-2026" cycleLength="32"/>
+        <Log date="11-12-2024" cycleLength="28"/>
+        <Log date="16-12-2024" cycleLength="26"/>
+        <Log date="13-12-2024" cycleLength="32"/>
     </View>
   );
 }
