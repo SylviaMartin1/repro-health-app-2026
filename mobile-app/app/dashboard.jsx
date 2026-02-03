8a352af17ead6613b436db92656ccb37f05b99be
sylvi <20102981@mail.wit.ie>
2026-02-03 10:42:03 +0000

    Add key property

--- mobile-app/app/dashboard.jsx
+++ mobile-app/app/dashboard.jsx
@@ -5,9 +5,9 @@ import Log from '../components/Log';
 export default function Dashboard() {
   const [ logState, setLogState] = useState({
     logs: [
-      { date: "11-12-2024", cycleLength:"28"},
-      { date: "16-12-2024", cycleLength:"26"},
-      { date: "13-12-2024", cycleLength:"32"}
+      { date: "11-12-2024", cycleLength:"28", id:1},
+      { date: "16-12-2024", cycleLength:"26", id:2},
+      { date: "13-12-2024", cycleLength:"32", id:3}
     ]
   })
 
@@ -15,9 +15,13 @@ export default function Dashboard() {
   return (
     <View>
         <Text>Dashboard</Text>
-        <Log date="11-12-2024" cycleLength="28"/>
-        <Log date="16-12-2024" cycleLength="26"/>
-        <Log date="13-12-2024" cycleLength="32"/>
+        {logState.logs.map((log) => (
+          <Log 
+            date={log.date}
+            cycleLength={log.cycleLength}
+            key={log.id}
+          />
+        ))}
     </View>
   );
 }
