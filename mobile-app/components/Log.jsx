016537d5c8292646651d818bdd0c0bcb92eaecc4
sylvi <20102981@mail.wit.ie>
2026-02-03 10:10:05 +0000

    Add the log component

---
+++ mobile-app/components/Log.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Log(){
+    
+    return(
+        <View>
+            <Text> This is a log !</Text>
+            
+        </View>
+    )
+}
