cf277fda9e30d4e321ced125c1db60fe64ff3209
sylvi <20102981@mail.wit.ie>
2026-02-03 10:25:19 +0000

    Add props to the log component

--- mobile-app/app/dashboard.jsx
+++ mobile-app/app/dashboard.jsx
@@ -5,7 +5,9 @@ export default function Dashboard() {
   return (
     <View>
         <Text>Dashboard</Text>
-        <Log/>
+        <Log date="11-12-2026" cycleLength="28"/>
+        <Log date="15-12-2026" cycleLength="26"/>
+        <Log date="13-12-2026" cycleLength="32"/>
     </View>
   );
 }
--- mobile-app/components/Log.jsx
+++ mobile-app/components/Log.jsx
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native';
 
-export default function Log(){
+export default function Log(props){
     
     return(
         <View>
-            <Text> This is a log !</Text>
-            
+            <Text>{props.date}</Text>
+            <Text>{props.cycleLength}</Text>
         </View>
     )
 }
