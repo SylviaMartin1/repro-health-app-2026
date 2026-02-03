c9e623555dd0f0c9f9189ac5aa075a47300d80b4
sylvi <20102981@mail.wit.ie>
2026-02-03 12:34:30 +0000

    Create the form

--- mobile-app/app/index.jsx
+++ mobile-app/app/index.jsx
@@ -33,6 +33,12 @@ export default function Index() {
                     style={{ width: 280, backgroundColor: colours.primary.main, paddingVertical: 10, paddingHorizontal: 20, borderRadius: 8, marginBottom: 10, alignItems: "center" }}>
                     <Text style={{ color: 'white', fontWeight: 'bold' }}>Dashboard</Text>
                 </Pressable>
+
+             <Pressable 
+                    onPress={() => router.push("/logging")}
+                    style={{ width: 280, backgroundColor: colours.primary.main, paddingVertical: 10, paddingHorizontal: 20, borderRadius: 8, marginBottom: 10, alignItems: "center" }}>
+                    <Text style={{ color: 'white', fontWeight: 'bold' }}>Logging</Text>
+                </Pressable>
         </View>
     );
 }
--- mobile-app/app/logging.jsx
+++ mobile-app/app/logging.jsx
@@ -1,14 +1,36 @@
 import { useState } from 'react';
-import { View, Text} from 'react-native'
+import { View, Text, TextInput} from 'react-native'
 
 export default function Logging() {
   const [date, setDate] = useState("")
   const [cycleLength, setCycleLength] = useState("")
 
-
   return (
     <View>
         <Text>Logging</Text>
+        <TextInput 
+          value={date}
+          onChangeText={setDate}
+          style={{
+          borderWidth: 1,
+          borderColor: '#ccc',
+          padding: 10,
+          borderRadius: 5,
+          marginTop: 5,
+        }}
+        />
+        <TextInput 
+          value={cycleLength}
+          onChangeText={setCycleLength}
+          style={{
+          borderWidth: 1,
+          borderColor: '#ccc',
+          padding: 10,
+          borderRadius: 5,
+          marginTop: 5,
+        }}
+        />
     </View>
+
   );
 }
